47f9864dab8d988ea65a8227303b8dfc9832cd45
mariiahoncharuk <mariia.honcharuk.kn.2021@lpnu.ua>
2024-10-23 00:10:31 +0200

    Initial Commit

---
+++ habitTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  habitTracker
+//
+//  Created by Mariia Honcharuk on 23.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ habitTrackerTests/habitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  habitTrackerTests.swift
+//  habitTrackerTests
+//
+//  Created by Mariia Honcharuk on 23.10.2024.
+//
+
+import Testing
+@testable import habitTracker
+
+struct habitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
